669277a4387035d02e7405dada3260f6479dcab6
Daniel Berger <djberg96@gmail.com>
2021-09-09 07:58:04 -0400

    Use different signals depending on OS.

--- lib/safe_timeout/spawner.rb
+++ lib/safe_timeout/spawner.rb
@@ -2,29 +2,32 @@ module SafeTimeout
   class Spawner
 
     def initialize(options={})
-      @expiration = Time.now.to_f + options.fetch(:timeout)
+      @expiration = Time.now.to_f + options.fetch(:timeout, 0)
       @on_timeout = options.fetch(:on_timeout)
+      @start_signal = File::ALT_SEPARATOR ? 'INT' : 'TRAP'
+      @stop_signal = File::ALT_SEPARATOR ? 'TERM' : 'HUP'
     end
 
     def start(&block)
-      original = Signal.trap('TRAP', &@on_timeout) || 'DEFAULT'
+      original = Signal.trap(@start_signal, &@on_timeout) || 'DEFAULT'
       spawn_interrupter
       yield
     ensure
-      Signal.trap('TRAP', original)
+      Signal.trap(@start_signal, original)
       stop
     end
 
     def stop
       # Tell that child to stop interrupting!
-      SafeTimeout.send_signal('HUP', @child_pid)
+      SafeTimeout.send_signal(@stop_signal, @child_pid)
     end
 
     def spawn_interrupter
       # Create a light-weight child process to notify this process if it is
       # taking too long
       bin = Gem.bin_path('safe_timeout', 'safe_timeout')
-      @child_pid = Process.spawn(bin, $$.to_s, @expiration.to_s)
+      @child_pid = Process.spawn(bin, Process.pid.to_s, @expiration.to_s)
+      puts "CHILD PID: #{@child_pid}"
       Process.detach(@child_pid)
     end
 

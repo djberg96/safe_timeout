a2eb762f9dc86d997f350b19583edc9af5f8f3bb
Daniel Berger <djberg96@gmail.com>
2021-09-09 07:58:42 -0400

    Use different signals depending on OS.

--- lib/safe_timeout/interrupting_child_process.rb
+++ lib/safe_timeout/interrupting_child_process.rb
@@ -4,17 +4,19 @@ module SafeTimeout
     def initialize(ppid, expiration)
       @ppid = ppid.to_i
       @expiration = expiration.to_f
+      @start_signal = File::ALT_SEPARATOR ? 'INT' : 'TRAP'
+      @stop_signal = File::ALT_SEPARATOR ? 'TERM' : 'HUP'
 
       abort "Invalid pid to monitor: #{@ppid}" if @ppid == 0
       abort "Invalid expiration: #{@expiration}" if @expiration == 0.0
     end
 
     def notify_parent_of_expiration
-      SafeTimeout.send_signal('TRAP', @ppid)
+      SafeTimeout.send_signal(@start_signal, @ppid)
     end
 
     def wait_for_timeout
-      Signal.trap('HUP') { exit 0 }
+      Signal.trap(@stop_signal) { exit 0 }
 
       sleep [@expiration - Time.now.to_f, 0.1].max
 
